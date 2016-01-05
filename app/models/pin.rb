643c663a525b080f6478e9a7aefce746a951e8b5
Nate Welling <primesitemedia@gmail.com>
2016-01-05 11:51:55 +0700

    add Name attribute to Users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ActiveRecord::Base
+  validates :image, presence: true
+  validates :description, presence: true
+
   belongs_to :user
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
